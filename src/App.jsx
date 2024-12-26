210bc4e8b2fb8092b0ffe29bd18c81dba36852ea
Ankit Chaturvedi <ankit.chaturvedi0201@gmail.com>
2024-12-26 13:05:30 +0530

    project upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Nav from './components/Nav.jsx'
+import Content from "./components/Content.jsx";
+
+function App() {
+
+
+  return (
+      <>
+          <Nav />
+          <Content />
+      </>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,1 @@
+export default function Nav(){    return(        <>            <nav className="bg-slate-800 text-slate-200 flex items-center justify-center h-20 gap-x-3">                <img src="" alt="logo"/>                <h2 className= "">Meme generator</h2>            </nav>        </>    )}
