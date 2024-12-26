e7cb08222832a67777f1929cd9fa61675cd38a7b
Ankit Chaturvedi <ankit.chaturvedi0201@gmail.com>
2024-12-26 13:21:34 +0530

    fixed line separator issue.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,1 +1,11 @@
-export default function Nav(){    return(        <>            <nav className="bg-slate-800 text-slate-200 flex items-center justify-center h-20 gap-x-3">                <img src="" alt="logo"/>                <h2 className= "">Meme generator</h2>            </nav>        </>    )}
+
+export default function Nav(){
+    return(
+        <>
+            <nav className="bg-slate-800 text-slate-200 flex items-center justify-center h-20 gap-x-3">
+                <img src="" alt="logo"/>
+                <h2 className= "">Meme generator</h2>
+            </nav>
+        </>
+    )
+}
